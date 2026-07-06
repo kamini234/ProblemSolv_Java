fdd6f8b07fee340029636f9ea9de785afe27b1ca
kamini234 <kaminimkp12@gmail.com>
2026-07-05 20:40:42 -0700

    Refactor Stack class to use java.util.Stack

--- Stack.java
+++ Stack.java
@@ -1,8 +1,7 @@
-
+import java.util.Stack;
 public class Stack{
-    //Create a Mathod
     public static void displayStack(){
-       java.util.Stack<Integer> numbers = new java.util.Stack<>();
+       Stack<Integer> numbers = new Stack<>();
         numbers.push(10);
         numbers.push(20);
         numbers.push(30);
@@ -10,9 +9,7 @@ public class Stack{
         System.out.println("Top Element:"+numbers.peek());
         System.out.println("Popped Element:"+numbers.pop());
         System.out.println("Stack after pop :"+numbers);
-
     }
-
     public static void main(String[] args) {
         displayStack();
     }
