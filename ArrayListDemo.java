95723c9d7412d7c49a6e5f3683a516a330a4e4c7
kamini234 <kaminimkp12@gmail.com>
2026-07-05 20:53:41 -0700

    Refactor ArrayListDemo.java formatting

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,7 +1,6 @@
 import java.util.ArrayList;
-
+import java.util.List;
 public class ArrayListDemo {
-    // Create a Method
     public static void displayList() {
         List<String> fruits = new ArrayList<>();
         fruits.add("Apple");
@@ -9,7 +8,6 @@ public class ArrayListDemo {
         fruits.add("Orange");
         System.out.println(fruits);
     }
-
     public static void main(String[] args) {
         displayList();
     }
