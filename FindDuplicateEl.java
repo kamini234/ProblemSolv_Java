25436f49b3fc58773871b016d432e3811c289705
kamini234 <kaminimkp12@gmail.com>
2026-07-05 20:29:11 -0700

    Update FindDuplicateEl.java

--- FindDuplicateEl.java
+++ FindDuplicateEl.java
@@ -4,8 +4,8 @@ import java.util.HashSet;
 //FindDuplicateElement
 public class FindDuplicateEl{
     public static void findDuplicate(int[] arr){
-HashSet<Integer> seen=new HashSet<>();
-HashSet<Integer> duplicates=new HashSet<>();
+Set<Integer> seen=new HashSet<>();
+Set<Integer> duplicates=new HashSet<>();
 for (int num:arr) {
 if(!seen.add(num)){
     duplicates.add(num);
