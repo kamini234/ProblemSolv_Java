60e5a842f50e2d1a8e2513d0bbf4bd1e6ac8f219
kamini234 <kaminimkp12@gmail.com>
2026-07-05 20:23:33 -0700

    Refactor BinarySearch class and remove comments

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,27 +1,4 @@
-//BinarySearch
-/*public class BinarySearch{
-    public static void main(String[] args) {
-        int[] arr={2,4,6,8,10,12};
-        int target=8;
-        int low=0;
-        int high =arr.length-1;
-        while(low <=high){
-            int mid=(low+high)/2;
-            if(arr[mid] ==target){
-                System.out.println("Found at index"+mid);
 
-                return;
-            }
-            if(arr[mid] <target){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        System.out.println("Target not found");
-    }
-}*/
 public class BinarySearch{
     public static int binarySearch(int[] arr, int target) {
         int low = 0;
