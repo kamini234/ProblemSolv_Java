7e804dd7bdc8502dfc68814246c22d5817956abf
kamini234 <kaminimkp12@gmail.com>
2026-07-05 20:52:32 -0700

    Refactor CountFrequencyEl to use Map interface

--- CountFrequencyEl.java
+++ CountFrequencyEl.java
@@ -1,20 +1,17 @@
 import java.util.HashMap;
+import java.util.Map;
 public class CountFrequencyEl{
     public static void countfrequency(int[] arr){
-        HashMap<Integer,Integer>map=new HashMap<>();
-       
+        Map<Integer,Integer>map=new HashMap<>();
         for(int num:arr){
             map.put(num,map.getOrDefault(num, 0)+1);
         }
-       
         for(int key:map.keySet()){ 
             System.out.println(key+"->"+map.get(key));
         }
     }
     public static void main(String[] args) {
-       
         int[] arr={1,2,3,4,5};
-     
         countfrequency(arr);
     }
 }
