a378e7d077acfa115379010f3a9a09cdd7a96ca6
kamini234 <kaminimkp12@gmail.com>
2026-07-05 20:42:28 -0700

    Refactor TreeMap to use Map interface

--- TreeMap.java
+++ TreeMap.java
@@ -1,14 +1,14 @@
-
+import java.util.TreeMap;
+import java.util.Map;
 public class TreeMap{
     
     public static void displayMap() {
-        java.util.TreeMap<Integer, String> map = new java.util.TreeMap<>();
+        Map<Integer, String> map = new TreeMap<>();
         map.put(1, "One");
         map.put(2, "Two");
         map.put(3, "Three");
         System.out.println(map);
     }
-
     public static void main(String[] args) {
         displayMap();
     }
